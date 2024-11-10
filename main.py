b26a606c86a695b2e14dad4ea1c93b623f8e6e8f
Justina Rhee <jrhee9@wisc.edu>
2024-11-10 10:55:42 -0600

    updated all files to match most current

--- main.py
+++ main.py
@@ -1,21 +1,73 @@
 import pandas as pd
-from flask import Flask, request, redirect, url_for, send_file
+from flask import Flask, request, redirect, url_for, send_file, render_template, session
 import csv
 import qrcode
 import os
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+import json
 
 app = Flask(__name__)
 
+# Load environment variables from .env file
+load_dotenv("env.env")
+
+# Debug prints to verify environment variables
+print("AUTH0_DOMAIN:", os.getenv("AUTH0_DOMAIN"))
+print("AUTH0_CLIENT_ID:", os.getenv("AUTH0_CLIENT_ID"))
+print("AUTH0_CLIENT_SECRET:", os.getenv("AUTH0_CLIENT_SECRET"))
+print("APP_SECRET_KEY:", os.getenv("APP_SECRET_KEY"))
+
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=os.getenv("AUTH0_CLIENT_ID"),
+    client_secret=os.getenv("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{os.getenv("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token.get("userinfo")
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    # Clear the session in Flask app
+    session.clear()
+
+    # Auth0 logout URL with return URL
+    auth0_logout_url = f"https://{os.getenv('AUTH0_DOMAIN')}/v2/logout?returnTo={url_for('home', _external=True)}&client_id={os.getenv('AUTH0_CLIENT_ID')}"
+    
+    # Redirect the user to the Auth0 logout URL
+    return redirect(auth0_logout_url)
+
+
 @app.route('/', methods=['GET'])
 def home():
-    with open("form.html") as f:
-        html = f.read()
-    return html
+    if 'user' in session:
+        return render_template("form.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    else:
+        return render_template("login.html")
 
 @app.route('/submit', methods=['POST'])
 def get_input():
     patient_dict = {}
-    
+
     patient_dict["First Name"] = request.form.get("fname")
     patient_dict["Middle Name"] = request.form.get("mname")
     patient_dict["Last Name"] = request.form.get("lname")
